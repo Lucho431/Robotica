f4336e874df86834b1e78776f6c97ddf81317052
Lucho_431 <54591018+Lucho431@users.noreply.github.com>
2022-03-29 00:43:19 -0300

    proyecto de pruebas en robot agregado

---
+++ pruebas_robot_STM32F429IGT6/Core/Inc/main.h
@@ -0,0 +1,92 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define PWM_Trig_Pin GPIO_PIN_10
+#define PWM_Trig_GPIO_Port GPIOH
+#define IC_Eco_Pin GPIO_PIN_12
+#define IC_Eco_GPIO_Port GPIOH
+#define PWM_R_Pin GPIO_PIN_12
+#define PWM_R_GPIO_Port GPIOD
+#define PWM_L_Pin GPIO_PIN_13
+#define PWM_L_GPIO_Port GPIOD
+#define OUT_in4_Pin GPIO_PIN_10
+#define OUT_in4_GPIO_Port GPIOC
+#define OUT_in3_Pin GPIO_PIN_11
+#define OUT_in3_GPIO_Port GPIOC
+#define OUT_in2_Pin GPIO_PIN_12
+#define OUT_in2_GPIO_Port GPIOC
+#define OUT_in1_Pin GPIO_PIN_0
+#define OUT_in1_GPIO_Port GPIOD
+#define IN_sensorL_Pin GPIO_PIN_1
+#define IN_sensorL_GPIO_Port GPIOD
+#define IN_sensorR_Pin GPIO_PIN_3
+#define IN_sensorR_GPIO_Port GPIOD
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
